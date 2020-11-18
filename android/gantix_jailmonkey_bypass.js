39e99b7c06c5cd34c5be0eeb2a0814cfec28c495
Terry Chia <terrycwk1994@gmail.com>
2020-11-18 11:09:56 +0800

    Add android/gantix_jailmonkey_bypass.js.

---
+++ android/gantix_jailmonkey_bypass.js
@@ -0,0 +1,19 @@
+/**
+Root detection bypass script for Gantix JailMoney
+https://github.com/GantMan/jail-monkey
+**/
+Java.perform(() => {
+    const klass = Java.use("com.gantix.JailMonkey.JailMonkeyModule");
+    const hashmap_klass = Java.use("java.util.HashMap");
+    const false_obj = Java.use("java.lang.Boolean").FALSE.value;
+
+    klass.getConstants.implementation = function () {
+        var h = hashmap_klass.$new();
+        h.put("isJailBroken", false_obj);
+        h.put("hookDetected", false_obj);
+        h.put("canMockLocation", false_obj);
+        h.put("isOnExternalStorage", false_obj);
+        h.put("AdbEnabled", false_obj);
+        return h;
+    };
+});
